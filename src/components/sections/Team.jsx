f5acc5cbd25291f67c3a7c56d426c193970b1340
A.V.Vignesh Chowdary <74393849+vicky2005-21@users.noreply.github.com>
2024-01-28 09:06:27 +0530

    Update Team.jsx

--- src/components/sections/Team.jsx
+++ src/components/sections/Team.jsx
@@ -1,15 +1,8 @@
 import styled from "styled-components";
 import ConfettiComponent from "../Confetti";
 
-import img1 from "../../assets/Nfts/bighead.svg";
-import img2 from "../../assets/Nfts/bighead-1.svg";
-import img3 from "../../assets/Nfts/bighead-2.svg";
-import img4 from "../../assets/Nfts/bighead-3.svg";
-import img5 from "../../assets/Nfts/bighead-4.svg";
+
 import img6 from "../../assets/Nfts/bighead-5.svg";
-import img7 from "../../assets/Nfts/bighead-6.svg";
-import img8 from "../../assets/Nfts/bighead-7.svg";
-import img9 from "../../assets/Nfts/bighead-8.svg";
 
 const Team = () => {
   return (
